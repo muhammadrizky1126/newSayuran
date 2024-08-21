372bb17972db59c34f9e1c93076cdc8457b50b0b
muhamadferi15 <mferifirmansyah15@gmail.com>
2024-08-21 12:23:49 +0700

    first commit

---
+++ app/Models/Product.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Product extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'category_id',
+        'product_name',
+        'weight_product',
+        'price',
+        'stock'
+    ];
+
+
+    public function categories()
+    {
+        return $this->belongsTo(Categories::class, 'category_id');
+    }
+
+
+    protected $casts = [
+        'price' => 'integer',
+        'weight_product' => 'integer',
+        'stock' => 'integer',
+    ];
+}
---
+++ database/migrations/2024_08_21_031358_create_orders_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger("user_id");
+            $table->integer("total_amount");
+            $table->enum("status", ["packing", "delivered", "order_received"]);
+            $table->bigInteger("shipping_address");
+            $table->bigInteger("payment_method");
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
